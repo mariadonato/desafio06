24448f7d72cb016fb67b12bde169fdddfc610e38
mariadonato <mariadelcielo34@gmail.com>
2023-12-21 19:17:52 -0300

    v0.0.2

--- js/main2.js
+++ js/main2.js
@@ -1,3 +1,18 @@
+console.error('Ejercicio B')
 
+var cantidadDeGatos = 5
+var cantidadDePasos = 3
+
+for (var i=1; i<=cantidadDeGatos; i++) {
+    var emoji = '🐈'
+
+    for (var n = 1; n<=cantidadDePasos; n++) {
+        emoji += '🐾'
+        
+    }
+
+    console.log('Gato #' + i + ': ' + emoji)
+    
+}
 
 
